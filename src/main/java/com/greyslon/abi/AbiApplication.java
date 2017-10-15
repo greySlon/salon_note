cff7b897f86ba52a136df6a6c47cd0bd974cafab
GreySlon <garanenkosergii@gmail.com>
2017-10-16 00:18:42 +0300

    Initial commit

---
+++ src/main/java/com/greyslon/abi/AbiApplication.java
@@ -0,0 +1,12 @@
+package com.greyslon.abi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbiApplication.class, args);
+	}
+}
